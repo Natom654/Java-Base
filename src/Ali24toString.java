fdd650ba9960185926c9cacd5f3f3d6f168d41cc
Omelchenko Nataly <natom654@gmail.com>
2022-07-06 08:47:51 +0200

    My first commit

---
+++ src/Ali24toString.java
@@ -0,0 +1,19 @@
+public class Ali24toString {
+    public static void main(String[] args) {
+        Human h1 = new Human("Bob", 40);
+        System.out.println(h1.toString());
+    }
+}
+class Human {
+    private String name;
+    private int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String toString() {
+        return name + "," + age;
+    }
+}
+
