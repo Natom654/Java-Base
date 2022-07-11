d86c18b9488b8fdc482da6919a97f5f5cba3fb85
Omelchenko Nataly <natom654@gmail.com>
2022-07-11 15:40:58 +0200

    First commit

---
+++ src/Ali25/Dog.java
@@ -0,0 +1,14 @@
+package Ali25;
+
+public class Dog extends Animal {
+    public void bark() {
+        System.out.println("Ali25.Dog is barking");
+    }
+
+    public void eat() {
+        System.out.println("Ali25.Dog is eating");
+    }
+    public void showName(){
+        System.out.println(name);
+    }
+}
