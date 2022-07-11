14f13c5e480a1cc933f0179f20434bce7f008ef8
Omelchenko Nataly <natom654@gmail.com>
2022-07-11 15:35:11 +0200

    First commit

---
+++ src/Ali26/Ali26Interface.java
@@ -0,0 +1,22 @@
+package Ali26;
+
+public class Ali26Interface {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal(1);
+        Person person1 = new Person("Bob");
+        animal1.sleep();
+        person1.sayHello();
+        animal1.showInfo();
+        person1.showInfo();
+        Info info1 = new Animal(2);
+        Info info2 = new Person("Rob");
+        outputInfo(animal1);
+        outputInfo(person1);
+        outputInfo(info1);
+        outputInfo(info2);
+    }
+
+    public static void outputInfo(Info info) {
+        info.showInfo();
+    }
+}
